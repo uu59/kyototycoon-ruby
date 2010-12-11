9776d0425ca88959ad85a8e4762365808723325c
uu59 <a@tt25.org>
2010-12-11 13:40:15 +0900

    Ignore nil value
    This is important fix for set/bulk_set with not set xt
    It causes all record expired at 0 sec(in other word, just a volatile)

--- lib/kyototycoon/tsvrpc.rb
+++ lib/kyototycoon/tsvrpc.rb
@@ -39,11 +39,17 @@ class KyotoTycoon
         case colenc.to_s.upcase.to_sym
           when :U
             query = params.inject([]){|r, tmp|
-              r << tmp.map{|v| CGI.escape(v.to_s)}.join("\t")
+              unless tmp.last.nil?
+                r << tmp.map{|v| CGI.escape(v.to_s)}.join("\t")
+              end
+              r
             }.join("\r\n")
           when :B
             query = params.inject([]){|r, tmp|
-              r << tmp.map{|v| Base64.encode64(v.to_s).rstrip}.join("\t")
+              unless tmp.last.nil?
+                r << tmp.map{|v| Base64.encode64(v.to_s).rstrip}.join("\t")
+              end
+              r
             }.join("\r\n")
           else
             raise "Unknown colenc '#{colenc}'"
